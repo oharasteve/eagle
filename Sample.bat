cac0ef7035944924cb850b6c3a3ff6e81f36e4f0
Steve O'Hara <steve@oharasteve.com>
2015-10-03 14:33:36 -0400

    Added tiny sample
    Just loads a small PHP file and prints the top level elements.

---
+++ Sample.bat
@@ -0,0 +1,4 @@
+@setlocal
+
+@set CP=bin
+@java -classpath "%CP%" Sample samples\www\xml\photos\pic_php.xml
---
+++ src/Sample.java
@@ -0,0 +1,40 @@
+import com.eagle.EagleReadXML;
+import com.eagle.programmar.PHP.PHP_Program;
+import com.eagle.programmar.PHP.PHP_Program.PHP_Entry;
+import com.eagle.tokens.AbstractToken;
+
+
+public class Sample
+{
+	private void process(String filename)
+	{
+		EagleReadXML reader = new EagleReadXML();
+		PHP_Program program = (PHP_Program) reader.readFrom(filename);
+		int i = 0;
+		for (PHP_Entry entry : program.entry._elements)
+		{
+			i++;
+			AbstractToken which = entry._whichToken;
+			System.out.println("** " + i + ": " + which.toString());
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		if (args.length < 1)
+		{
+			System.out.println("Usage: file_php.xml");
+			System.exit(1);
+		}
+		String filename = args[0];
+		if (! filename.endsWith("_php.xml"))
+		{
+			System.err.println("Please use a PHP file, not " + filename);
+			System.exit(2);
+		}
+		
+		Sample s = new Sample();
+		s.process(filename);
+		System.exit(0);
+	}
+}
